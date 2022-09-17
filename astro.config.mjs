6821db7b13d43e26d45f1fe32ed0f4bd450c37d9
Dan Ferguson <thedanielfergusonkid@gmail.com>
2022-09-17 11:29:41 +1000

    📦 added robots.txt to build step

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danferg.com',
-  integrations: [tailwind(), image(), sitemap(), mdx()],
+  integrations: [tailwind(), image(), sitemap(), mdx(), robotsTxt()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   }
