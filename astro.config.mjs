8428dd2ef80b4c54be3783542bdf299f802d8bd6
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-12-09 14:56:39 +1100

    removed trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './remark-reading-time.mjs';
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from 'astro-robots-txt';
-
+import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://danferg.com',
+  site: "https://danferg.com",
   integrations: [tailwind(), image(), sitemap(), mdx(), robotsTxt()],
   markdown: {
-    remarkPlugins: [remarkReadingTime]
-  }
+    remarkPlugins: [remarkReadingTime],
+  },
+  trailingSlash: "never",
 });
